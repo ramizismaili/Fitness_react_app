4ce6e92d69d5c1b8cb4c3ccf43a3238800afe1e6
Ramiz Ismaili <rismaili49@gmail.com>
2022-11-20 21:52:05 +0100

    Bodypart & Exercises(Card) Components

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material/'
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [ search, setSearch] = useState('');
-  const [exercises, setExercises] = useState([])
+
   const [bodyParts, setBodyParts] = useState([])
 
 useEffect(() => {
   const fetchExercisesData = async () => {
-    const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartsList', exerciseOptions)
+    const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
 
     setBodyParts(['all', ...bodyPartsData]);
   }
@@ -79,7 +80,7 @@ useEffect(() => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   )
